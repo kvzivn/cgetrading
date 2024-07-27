d6225dff023f76ffae810c56877fb9dbe05e019b
cgetrading <studentofsoc@icloud.com>
2024-07-27 19:44:31 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -200,7 +200,7 @@ const App = () => {
                       <br /> Play Indicator
                     </h2>
                     <p className="text-xl text-gray-300">
-                      We are excited to announce that The Trap Play Indicator, a
+                      We are excited to announce that the Trap Play Indicator, a
                       top-notch liquidity pattern tool, is almost ready for
                       launch on the TradingView platform. Sign up for the
                       waiting list now to be among the first to access this
