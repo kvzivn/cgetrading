95aaaaf32961906972ec014621f824d1fd31b403
cgetrading <studentofsoc@icloud.com>
2024-07-31 22:18:29 +0200

    CHART BYTE IGEN

--- src/TradingViewChart.tsx
+++ src/TradingViewChart.tsx
@@ -19,7 +19,7 @@ const TradingViewChart: React.FC = () => {
       if (window.TradingView) {
         new window.TradingView.chart({
           container_id: "tradingview_chart",
-          chart: "cNmqa5QY",
+          chart: "ky5wsKTC",
           autosize: true,
         })
       }
