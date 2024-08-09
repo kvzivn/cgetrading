9301cbd0947f920a5d29da55d69ce92d0c9bbec3
cgetrading <studentofsoc@icloud.com>
2024-08-09 08:06:43 +0200

    Update TradingViewDCF.tsx

--- src/TradingViewDCF.tsx
+++ src/TradingViewDCF.tsx
@@ -19,7 +19,7 @@ const TradingViewDCF: React.FC = () => {
       if (window.TradingView) {
         new window.TradingView.chart({
           container_id: "tradingview_chart_dcf",
-          chart: "l2KwpTTE",
+          chart: "OWzvG3s8",
           autosize: true,
         })
       }
