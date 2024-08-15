588f4ed209b8debbe4b0ddc63d22e66afc7ad268
cgetrading <studentofsoc@icloud.com>
2024-08-15 11:42:53 +0200

    Update TradingViewChart.tsx

--- src/TradingViewChart.tsx
+++ src/TradingViewChart.tsx
@@ -19,7 +19,7 @@ const TradingViewChart: React.FC = () => {
       if (window.TradingView) {
         new window.TradingView.chart({
           container_id: "tradingview_chart",
-          chart: "ky5wsKTC",
+          chart: "0vXNwDfu",
           autosize: true,
         })
       }
