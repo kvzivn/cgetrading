c238e58bb69800556ea6d6c28e719de4074805cc
Kevin <hello@kevinivan.com>
2024-08-30 23:36:09 +0200

    simplify

--- src/App.tsx
+++ src/App.tsx
@@ -100,32 +100,34 @@ const App = () => {
       ctaText: "Sign up",
       link: "https://refer.ig.com/carlgustave-6",
     },
-    // {
-    //   logo: "/ctrader.png",
-    //   heading: "Join my $100K USD challenge",
-    //   text: "Subscribe to DCF Bets with cTrader Copy and watch your portfolio soar. Here's the deal:",
-    //   items: [
-    //     "Performance fee: 25%",
-    //     "Management fee: 5%",
-    //     "Let's make some serious gains together!",
-    //   ],
-    //   ctaText: "Join challenge",
-    //   link: "",
-    // },
   ]
 
   return (
     <HeroHighlight>
-      <div className="pt-10 pb-16">
+      <div className="pb-16">
         <Spotlight
           className="hidden lg:block -left-[27.5rem] -top-[70rem]"
           fill="white"
         />
 
+        <div className="md:hidden relative mb-8 mt-6 md:mt-0">
+          <Ticker />
+        </div>
+
+        <div className="hidden md:block relative mb-8">
+          <div className="absolute top-0 left-0 w-40 h-[47px] bg-gradient-to-r from-black to-transparent" />
+          <div>
+            <Ticker />
+          </div>
+          <div className="absolute top-0 right-0 w-40 h-[47px] bg-gradient-to-l from-black to-transparent " />
+        </div>
+
         <div className="w-full relative flex flex-col items-center justify-center">
-          <div className="flex flex-col z-10 items-center justify-center w-full py-12 md:py-24 lg:pb-8 lg:pt-0 text-slate-300">
+          <div className="flex flex-col z-10 items-center justify-center w-full pt-0 pb-12 md:py-24 lg:pb-8 lg:pt-0 text-slate-300">
             <div className="px-4 md:px-6 text-center">
-              <div className="space-y-4 mb-8">
+              <img src="/logo2.png" alt="logo" className="h-16 w-16 mx-auto" />
+
+              <div className="space-y-4 mt-4 mb-8">
                 <FadeIn delay={600}>
                   <h1 className="text-4xl max-w-xl font-bold tracking-tighter sm:text-5xl md:text-6xl bg-gradient-to-l from-purple-400 via-purple-300 to-white inline-block text-transparent bg-clip-text">
                     Navigate the Evolving Gold & Forex Market
@@ -171,139 +173,52 @@ const App = () => {
               </div>
 
               <FadeIn delay={1200}>
-                <p className="max-w-[38rem] mx-auto text-muted-foreground md:text-xl mt-10">
-                 Get ready to supercharge your trading game! This model helps you effortlessly capitalize on minimal resistance and market trends. Guided by a score range of -7 to +7, it ensures your trading decisions are both savvy and effective, while also alerting you to potential liquidity traps.
+                <p className="max-w-[44rem] mx-auto text-muted-foreground md:text-xl mt-14">
+                  Get ready to supercharge your trading game! This model helps
+                  you effortlessly capitalize on minimal resistance and market
+                  trends. Guided by a score range of -7 to +7, it ensures your
+                  trading decisions are both savvy and effective, while also
+                  alerting you to potential liquidity traps.
                 </p>
               </FadeIn>
 
               <FadeIn delay={1400}>
-                <div className="mt-10 flex flex-col gap-16 sm:flex-row items-center sm:justify-center">
-                  <DiscordBtn />
+                <div className="mt-10 flex flex-col gap-12 sm:flex-row items-center sm:justify-center">
+                  <a
+                    href="https://www.mql5.com/en/users/411623/seller"
+                    target="_blank"
+                    className="inline-flex w-64 h-16 items-center justify-center px-8 font-medium text-lg text-white bg-purple-700 rounded-xl shadow transition-colors hover:text-white hover:border-transparent hover:bg-purple-600 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                  >
+                    Visit my{" "}
+                    <img
+                      src="/mql5btn.png"
+                      alt="mql5"
+                      className="h-8 rounded-full mx-1"
+                    />{" "}
+                    store
+                  </a>
+
+                  <a
+                    href="https://www.mql5.com/en/market/product/101755"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex w-64 h-16 items-center justify-center rounded-xl bg-white text-black px-6 text-lg font-medium text-primary-foreground shadow transition-colors hover:bg-purple-200 hover:text-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                  >
+                    Buy my{" "}
+                    <img
+                      src="/quant-logo.png"
+                      alt="quant"
+                      className="h-7 mr-1.5 ml-2"
+                    />{" "}
+                    TrapBot
+                  </a>
                 </div>
               </FadeIn>
             </div>
           </div>
         </div>
 
-        {/* mobile ticker */}
-        <div className="md:hidden relative mb-8 mt-6 md:mt-0">
-          <Ticker />
-        </div>
-
-        <div className="px-6 sm:px-16 xl:px-32">
-          <section className="w-full py-12">
-            {/* desktop ticker */}
-            <div className="hidden md:block relative mb-14">
-              <div className="absolute top-0 left-0 w-40 h-[47px] bg-gradient-to-r from-black to-transparent" />
-              <div>
-                <Ticker />
-              </div>
-              <div className="absolute top-0 right-0 w-40 h-[47px] bg-gradient-to-l from-black to-transparent " />
-            </div>
-
-            <div>
-              <div className="container mx-auto mb-12 grid grid-cols-1 xl:grid-cols-2 gap-12 p-8 md:py-12 xl:py-16 xl:px-16 lg:gap-24 border border-slate-700 bg-[#170e27]/90 rounded-xl">
-                <div className="grid place-items-center">
-                  <div>
-                    <h2 className="text-4xl mb-6 font-bold tracking-tighter md:text-6xl bg-gradient-to-r from-purple-500 via-purple-300 to-white inline-block text-transparent bg-clip-text">
-                      Unveiling The Trap
-                      <br /> Play Indicator
-                    </h2>
-                    <p className="text-xl text-gray-300 max-w-lg">
-                      We’re excited to announce that the Trap Play Indicator,
-                      a premium liquidity tool, is now ready for launch on the
-                      TradingView platform. Be among the first to access this
-                      exclusive tool.
-                    </p>
-                    <div className="flex gap-4 mt-8">
-                      <a
-                        href="https://www.tradingview.com/script/ky5wsKTC-The-Trap-Play/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="inline-flex h-12 items-center justify-center rounded-xl bg-white text-black px-6 text-lg font-medium text-primary-foreground shadow transition-colors hover:bg-purple-200 hover:text-black focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-                      >
-                        Explore
-                      </a>
-                      <a
-                        href="https://whop.com/cge-trading/?pass=prod_op7Sw860geSzs"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="inline-flex h-12 items-center justify-center rounded-xl bg-purple-700 text-white px-6 text-lg font-medium text-primary-foreground shadow transition-colors hover:bg-purple-800 hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-                      >
-                        7-day free trial
-                      </a>
-                    </div>
-                  </div>
-                </div>
-
-                <div>
-                  <TradingViewChart />
-                </div>
-              </div>
-            </div>
-
-            <div>
-              <div className="container mx-auto mb-12 flex flex-col-reverse xl:flex-row gap-12 p-8 md:py-12 xl:py-16 xl:px-16 lg:gap-2 border border-slate-700 bg-[#170e27]/90 rounded-xl">
-                <div className="w-full">
-                  <TradingViewDCF />
-                </div>
-                <div className="grid place-items-center w-full">
-                  <div>
-                    <h2 className="text-4xl mb-6 font-bold tracking-tighter md:text-[3.75rem] md:leading-[1.15] bg-gradient-to-r from-purple-500 via-purple-300 to-white inline-block text-transparent bg-clip-text">
-                      DCF Bets
-                      <br /> on TradingView
-                    </h2>
-                    <p className="mt-2 text-xl text-purple-400/90">
-                      Follow me on TradingView for exclusive access to:
-                    </p>
-                    <ul className="mt-6">
-                      <li className="flex items-center mt-1 text-slate-300/90 text-lg">
-                        <span className="bullet text-2xl mr-2">&#8226;</span>{" "}
-                        Consistent profits with my proven capital flow
-                        strategies
-                      </li>
-                      <li className="flex items-center mt-1 text-slate-300/90 text-lg">
-                        <span className="bullet text-2xl mr-2">&#8226;</span>{" "}
-                        Valuable market insights through detailed trade analysis
-                      </li>
-
-                      <li className="flex items-center mt-1 text-slate-300/90 text-lg">
-                        <span className="bullet text-2xl mr-2">&#8226;</span>{" "}
-                        Daily trade picks to give you an edge in the market
-                      </li>
-                    </ul>
-                    <div className="mt-8">
-                      <a
-                        href="https://www.tradingview.com/u/CGE_Trading/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="inline-flex h-12 items-center justify-center rounded-xl bg-purple-700 text-white px-6 text-lg font-medium text-primary-foreground shadow transition-colors hover:bg-purple-800 hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-                      >
-                        Follow me
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 lg:gap-12">
-              {cardsData.map((card, index) => (
-                <Card
-                  key={index}
-                  logo={card.logo}
-                  heading={card.heading}
-                  items={card.items}
-                  text={card.text}
-                  ctaText={card.ctaText}
-                  link={card.link}
-                />
-              ))}
-            </div>
-          </section>
-        </div>
-
-        <div className="mt-20 w-full text-center text-slate-600 py-4">
+        <div className="mt-4 sm:mt-16 w-full text-center text-slate-600 py-4">
           <p>
             &copy; {new Date().getFullYear()} CGE Trading. All rights reserved.
           </p>
