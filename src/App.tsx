19b14bfaec4c1f96948a17f2872ede2b8c0d2744
cgetrading <studentofsoc@icloud.com>
2024-12-25 21:01:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -190,7 +190,7 @@ const App = () => {
                         transition: { delay: 0.8, duration: 0 },
                       }}
                     />
-                    Navigate the Evolving Markets
+                    Navigate Success, Powered by Capital Flows
                     <motion.img
                       src="/bear.svg"
                       alt="logo"
