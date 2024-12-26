97d4cf95d84348d034f1c779fa8201556d181961
cgetrading <studentofsoc@icloud.com>
2024-12-26 14:47:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -190,7 +190,7 @@ const App = () => {
                         transition: { delay: 0.8, duration: 0 },
                       }}
                     />
-                    Navigate Success by Capital Flows
+                    Navigate Success by Capital Flow$
                     <motion.img
                       src="/bear.svg"
                       alt="logo"
