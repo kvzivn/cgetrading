4f86baf3138bbdccd6d46e54355717dd8c48329e
cgetrading <studentofsoc@icloud.com>
2025-09-04 11:49:32 +0200

    canva 2

--- src/App.tsx
+++ src/App.tsx
@@ -190,7 +190,7 @@ const App = () => {
                         <iframe
                           className="absolute top-0 left-0 w-full h-full border-0 p-0 m-0"
                           loading="lazy"
-                          src="https://www.canva.com/design/DAGNS4CBz04/XT3htlX6mVdNMQOXk3PpcA/view"
+                          src="https://www.canva.com/design/DAGNS4CBz04/XT3htlX6mVdNMQOXk3PpcA/view?embed"
                           allow="fullscreen"
                           onLoad={() => {
                             // This ensures the iframe loads after everything else
@@ -201,7 +201,7 @@ const App = () => {
                               }
                             })
                           }}
-                          data-src="https://www.canva.com/design/DAGI-sMGDBs/H8fsyHF4FIeLoGGDILZcjg/view?embed"
+                          data-src="https://www.canva.com/design/DAGNS4CBz04/XT3htlX6mVdNMQOXk3PpcA/view?embed"
                         ></iframe>
                       </div>
                     </BackgroundGradient>
